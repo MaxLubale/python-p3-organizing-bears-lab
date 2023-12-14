02e9d2374369fc43efafd09517ab2e355fffe5e4
Maxwell <lubalecliff@gmail.com>
2023-12-14 12:29:08 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+-- Inserting bears into the "bears" table
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+('Tabitha', 4, 'F', 'Gray', 'Calm', 1),
+('Sergeant Brown', 6, 'M', 'Brown', 'Disciplined', 1),
+('Melissa', 2, 'F', 'White', 'Playful', 1),
+('Grinch', 7, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 8, 'F', 'Brown', 'Sweet', 1),
+(NULL, NULL, NULL, NULL, NULL, NULL);  
